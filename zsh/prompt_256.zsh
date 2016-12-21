75756415b8e56565f52db6c61761b4fb0f86237c
Florian Anderiasch <florian.anderiasch@wywy.com>
2016-12-21 10:39:19 +0100

    Add HOSTCOLOR/USERCOLOR

--- zsh/prompt_256.zsh
+++ zsh/prompt_256.zsh
@@ -32,24 +32,34 @@ fi
 
 HOSTNAME=$HOST
 shortname=${HOSTNAME%%.*}
-hostname_crc=$(echo $HOSTNAME | tr 'A-Z' 'a-z' | cksum)
-hostname_crc=${hostname_crc%% *}
-hostcolor=$(( 0x${hostname_crc} % 8 + 104 ))
 
-hostname_crc=$(echo $HOSTNAME | tr 'A-Z' 'a-z' | cksum | cut -c-10)
-hostcolor=$(($hostname_crc % 8))
+if [ -n "$HOSTCOLOR" ]; then
+  hostcolor="$HOSTCOLOR";
+else
+  hostname_crc=$(echo $HOSTNAME | tr 'A-Z' 'a-z' | cksum)
+  hostname_crc=${hostname_crc%% *}
+  hostcolor=$(( 0x${hostname_crc} % 8 + 104 ))
 
-local p="%{$FX[reset]$FG[243]%}"
+  hostname_crc=$(echo $HOSTNAME | tr 'A-Z' 'a-z' | cksum | cut -c-10)
+  hostcolor=$(($hostname_crc % 8))
+fi
+if [ -n "$USERCOLOR" ]; then
+  usercolor="$USERCOLOR";
+else
+  usercolor="220"
+fi
+
+local px="%{$FX[reset]$FG[243]%}"
 #local name="%{$FX[reset]$FG[114]%}%n" # lgreen
-local name="%{$FX[reset]$FG[220]%}%n"
+local name="%{$FX[reset]$FG[$usercolor]%}%n"
 local host="%{$FX[reset]$FG[$hostcolor]%}%m"
-#local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${p})-.)"
+#local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${px})-.)"
 #local time="%{$FX[reset]$FG[215]%}%*"
 local dir="%{$FX[reset]$FG[215]%}%3~"
 
-local last="%(?..%{$FX[reset]$FG[203]%}%??${p})"
+local last="%(?..%{$FX[reset]$FG[203]%}%??${px})"
 local last2=`prefixlambda`
-#local hist="%{$FX[reset]$FG[220]%}%!!"
+#local hist="%{$FX[reset]$FG[$usercolor]%}%!!"
 #local priv="%{$FX[reset]$FG[245]%}%#"
 local sign="%{$FX[reset]$FG[117]%}$"
 
@@ -57,7 +67,7 @@ local sign="%{$FX[reset]$FG[117]%}$"
 zstyle ':vcs_info:*' enable git svn
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr "%{$FX[reset]$FG[082]%}"
-zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[160]%}"
+zstyle ':vcs_info:*' unstagedstr "%{$FX[reset]$FG[197]%}"
 zstyle ':vcs_info:*' formats ":%{$FX[reset]$FG[222]%}%c%u%b"
 
 local vcsi='${vcs_info_msg_0_}'
@@ -67,4 +77,4 @@ if [[ "$LC_PUTTY" -eq 1 ]]; then
 fi
 
 setopt prompt_subst
-PROMPT="${last2}${p}(${name}${p}@${host}${p} ${dir}${p}${vcsi}${p}) ${sign} %{$FX[reset]%}"
+PROMPT="${last2}${px}(${name}${px}@${host}${px} ${dir}${px}${vcsi}${px}) ${sign} %{$FX[reset]%}"
